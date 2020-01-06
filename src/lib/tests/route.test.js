344296e8e967c7c4d251ccf4f43cfbc6c31ccf2e
Eddie Padin <eddie@entertainermedia.net>
2020-01-06 14:10:20 -0600

    - test: update route tests;

--- src/lib/tests/route.test.js
+++ src/lib/tests/route.test.js
@@ -2,41 +2,25 @@ import route from "../route";
 import MockAdapter from "axios-mock-adapter";
 import axios from "axios";
 
-describe("route tests", () => {
-  it("should only parse path if string", () => {
-    const pathObj = {};
-    expect(route(pathObj)).toMatchObject(pathObj);
+let mock;
 
-    const pathStr = "/path/:param1";
-    expect(route(pathStr)).toEqual(/\/path\/[^\/]+/);
-  });
+beforeAll(() => {
+  mock = new MockAdapter(axios);
+});
 
-  it("should replace params in the path", () => {
-    const mock = new MockAdapter(axios);
-    const path = "/path/:param1";
-    const url = "/path/43";
+describe("when route is requested", () => {
+  const path = "/path/:param1/:param2";
+  const url = "/path/43/title";
 
+  it("should replace params in the path and set the wildcards as request params", () => {
     mock.onGet(route(path)).reply(request => {
-      expect(request.url).toEqual(url);
-      return [200, request];
-    });
+      expect(request.url).toMatch(url);
 
-    expect(axios.get(url)).resolves.toEqual(
-      expect.objectContaining({ status: 200 })
-    );
-  });
-
-  it("should set the wildcards as request params", () => {
-    const mock = new MockAdapter(axios);
-    const path = "/path/:param1/:param2";
-    const url = "/path/43/title";
-
-    mock.onGet(route(path)).reply(request => {
       expect(request.params).toEqual(
         expect.objectContaining({ param1: 43, param2: "title" })
       );
 
-      return [200];
+      return [200, request];
     });
 
     expect(axios.get(url)).resolves.toEqual(
